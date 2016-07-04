82a3847e26fee607c7375d834adfa250efa650bc
Jonathan Walker <xorith@gmail.com>
2016-07-04 12:39:01 -0400

    Added $domain for security

--- php/stats.php
+++ php/stats.php
@@ -7,8 +7,12 @@
 // $pathToStats = "/home/jonathan/stats.xml"; // Local 
 $pathToStats = "http://brain.death.rocks/~jonathan/stats.xml"; // URL
 
+// Your Domain
+// For security, change * to your website's domain.
+$domain = "*";
+
 // DO NOT ALTER PAST THIS POINT UNLESS YOU KNOW WHAT YOU ARE DOING
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: {$domain}");
 if(($stats = file_get_contents($pathToStats)) === false) {
     header("HTTP/1.0 500 Server Error: {$pathToStats} is not readable.");
 } else {
